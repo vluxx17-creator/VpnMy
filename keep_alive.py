c4df86f9184d25a74e28f5af3a80eabe12a85a49
DiMaggio <vluxx17@gmail.com>
2026-05-06 21:39:02 +0300

    Simplify home message and thread creation

--- keep_alive.py
+++ keep_alive.py
@@ -2,14 +2,8 @@ from flask import Flask
 from threading import Thread
 
 app = Flask('')
-
 @app.route('/')
-def home():
-    return "Fly VPN is running!"
+def home(): return "Fly VPN @tgxel is online"
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+def run(): app.run(host='0.0.0.0', port=8080)
+def keep_alive(): Thread(target=run).start()
